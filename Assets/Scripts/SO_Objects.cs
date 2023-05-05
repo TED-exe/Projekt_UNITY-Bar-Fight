8a965170addd60346a371bca98a9adc1e8ff4ebc
Retsav <ddadanski@edu.cdv.pl>
2023-05-05 19:54:23 +0200

    ScriptableObjects for throwable objects

---
+++ Assets/Scripts/SO_Objects.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "New throwable object", menuName = "ScriptableObjects/NewThrowableObject")]
+public class SO_Objects : ScriptableObject
+{
+    public Transform prefab;
+    
+
+}
