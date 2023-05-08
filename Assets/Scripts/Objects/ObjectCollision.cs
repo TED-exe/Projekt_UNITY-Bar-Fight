e31a52fb9948fcfcf17803df3783a3c0a54fb868
Retsav <ddadanski@edu.cdv.pl>
2023-05-08 22:06:10 +0200

    Added detecting collision with player

--- Assets/Scripts/Objects/ObjectCollision.cs
+++ Assets/Scripts/Objects/ObjectCollision.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ObjectCollision : MonoBehaviour
 {
@@ -10,7 +11,8 @@ public class ObjectCollision : MonoBehaviour
     private const int ENVIRONMENT_LAYER_NUMBER = 6;
     private const int THROWED_LAYER_NUMBER = 7;
     private const int PICKABLE_LAYER_NUMBER = 3;
-    
+
+    public UnityEvent onPlayerHit;
     private void OnCollisionEnter(Collision other)
     {
         CheckDestruction(other);
@@ -21,6 +23,7 @@ public class ObjectCollision : MonoBehaviour
         if (other.gameObject.CompareTag(tag: "Player") && gameObject.layer == THROWED_LAYER_NUMBER)
         {
             StartCoroutine(DestroyObject());
+            onPlayerHit.Invoke();
         }
         else if (other.gameObject.layer == ENVIRONMENT_LAYER_NUMBER && gameObject.layer == THROWED_LAYER_NUMBER)
         {
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public void OnHit()
+    {
+        Debug.Log("Player dead");
+    }
+}
