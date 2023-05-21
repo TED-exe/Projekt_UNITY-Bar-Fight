f552fe889ff4eeb93484c6b1c43033d9182a049e
Retsav <ddadanski@edu.cdv.pl>
2023-05-21 18:31:46 +0200

    Added delegate to RagdollPlayer script

--- Assets/Scripts/Systems/PlayerDeathLogic.cs
+++ Assets/Scripts/Systems/PlayerDeathLogic.cs
@@ -2,29 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerDeathLogic: MonoBehaviour
 {
-    public delegate void Hit();
-    public static Hit OnHit;
     private const int THROWED_LAYER = 7;
     [SerializeField] private Transform graveyard;
+    [SerializeField] private float ragdollSpeed = 10f;
     private PlayerForcefield playerForcefield;
+    private ThrowSystem throwSystem;
 
     [SerializeField] private int deathCounter;
     public bool isHit = false;
+    public Vector3 collisionVelocity;
 
     public delegate void sendToUI(GameObject player, int deaths);
     public static sendToUI sendUI;
+    
+    public delegate void Hit();
+    public static Hit OnHit;
+
+    public delegate void ragdoll(Vector3 objectVelocity);
+    public static ragdoll sendToRagdoll;    
+    
+    [SerializeField] private SO_Ragdoll playerRagdoll;
 
 
     private void OnEnable()
     {
         PlayerSpawnManager.OnSpawned += SpawnPlayer;
         playerForcefield = GetComponent<PlayerForcefield>();
+        throwSystem = GetComponent<ThrowSystem>();
     }
 
     public void SpawnPlayer(GameObject spawnPoint)
@@ -40,12 +51,15 @@ public class PlayerDeathLogic: MonoBehaviour
             if (isHit) return;
             //other.gameObject.transform.position = graveyard.position;
             isHit = true;
+            collisionVelocity = other.relativeVelocity.normalized * ragdollSpeed;
+            Instantiate(playerRagdoll.prefabRagdoll, transform.position, transform.rotation);
             this.GameObject().SetActive(false);
             playerForcefield.enabled = true;
             deathCounter++;
-           OnHit?.Invoke();
+            OnHit?.Invoke();
+            sendToRagdoll?.Invoke(collisionVelocity);
             sendUI?.Invoke(this.gameObject.transform.parent.gameObject, deathCounter);
-           GetComponentInParent<RespawnPlayer>().StartSpawning();
+            GetComponentInParent<RespawnPlayer>().StartSpawning();
         }
     }
 }
