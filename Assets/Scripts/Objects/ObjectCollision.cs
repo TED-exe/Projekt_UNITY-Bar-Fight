398acac347ca383f563de8e7991a8be8ec818e36
TED-dom <wojcikkuba414@gmail.com>
2023-06-06 00:00:55 +0200

    addAudio

--- Assets/Scripts/Objects/ObjectCollision.cs
+++ Assets/Scripts/Objects/ObjectCollision.cs
@@ -6,12 +6,6 @@ public class ObjectCollision : MonoBehaviour
     private const int ENVIRONMENT_LAYER_NUMBER = 6;
     private const int THROWED_LAYER_NUMBER = 7;
     private const int PICKABLE_LAYER_NUMBER = 3;
-    [SerializeField] private AudioClip[] _glassBreakingSounds;
-    [SerializeField] private AudioSource _soundsSource;
-    [Range(0.1f,0.5f)]
-    [SerializeField] private float _soundsVolumMultiplayer;
-    [Range(0.1f, 0.5f)]
-    [SerializeField] private float _soundsPitchMultiplayer;
 
     public UnityEvent onPlayerHit;
     private void OnCollisionEnter(Collision other)
@@ -29,12 +23,5 @@ public class ObjectCollision : MonoBehaviour
         {
             gameObject.layer = PICKABLE_LAYER_NUMBER;
         }
-    }
-    public void RandomizeGlassBreakingSounds()
-    {
-        _soundsSource.clip = _glassBreakingSounds[Random.Range(0,_glassBreakingSounds.Length)];
-        _soundsSource.volume = Random.Range(1 - _soundsVolumMultiplayer, 1);
-        _soundsSource.pitch = Random.Range(1 - _soundsPitchMultiplayer, 1 + _soundsPitchMultiplayer);
-        _soundsSource.PlayOneShot(_soundsSource.clip);
     }
 }
